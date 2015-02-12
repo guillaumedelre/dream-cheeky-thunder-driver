a1e62b1029fc997e0009dc4e64040b2931a1f992
jack kwong <jack.kwong@gogotech.hk>
2015-02-12 19:08:42 +0800

    fixed consecutive firing problem

--- scripts/signal.js
+++ scripts/signal.js
@@ -2,13 +2,25 @@ define(['underscore', 'launcherUSB', 'deviceConstants'], function(__, launcher, 
   return function signal(cmd, durationMS, callback) {
     launcher.controlTransfer(0x21, 0x09, 0x0, 0x0, new Buffer([0x02, cmd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
         function (data) {
+          var movementCommand = [
+            DEVICE_CONSTANTS.CMD.UP,
+            DEVICE_CONSTANTS.CMD.DOWN,
+            DEVICE_CONSTANTS.CMD.LEFT,
+            DEVICE_CONSTANTS.CMD.RIGHT
+          ];
+
           if (!__.isNumber(durationMS)) return;
           if (durationMS <= 0) {
             if (__.isFunction(callback)) callback();
             return;
           }
 
-          __.delay(function(){ 
+          if (!__.contains(movementCommand, cmd)) {
+            __.delay(callback, durationMS);
+            return;
+          }
+
+          __.delay(function(){
             if(__.isFunction(callback)) callback();
             signal(DEVICE_CONSTANTS.CMD.STOP);
           }, durationMS);
